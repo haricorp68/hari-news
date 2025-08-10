1fc27141b92aba96e94aa3c8951275fe65242578
Hoanghaaii <haivu04112003@gmail.com>
2025-08-10 14:04:37 +0700

    fix: use cloud db

--- src/data-source.ts
+++ src/data-source.ts
@@ -29,11 +29,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_DATABASE || 'hari_news',
+  url: process.env.DATABASE_URL,
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
   entities: [
     User,
